4d001456be17cd4aa34a5bcab53065059769cc85
sbezanovic <sbezanovic@griddynamics.com>
2023-03-03 14:14:51 +0100

    initial commit 2

--- linux-vm-main.tf
+++ linux-vm-main.tf
@@ -39,3 +39,25 @@ resource "google_compute_instance" "vm_instance_public" {
     access_config { }
   }
 } 
+
+resource "google_compute_instance" "vm_instance_public2" {
+  name         = "${lower(var.company)}-${lower(var.app_name)}-${var.environment}-vm${random_id.instance_id.hex}-2"
+  machine_type = var.linux_instance_type
+  zone         = var.gcp_zone
+  hostname     = "${var.app_name}-vm${random_id.instance_id.hex}.${var.app_domain}"
+  tags         = ["ssh","http"]
+
+
+  boot_disk {
+    initialize_params {
+      image = var.ubuntu_1804_sku
+    }
+  }
+  metadata_startup_script = data.template_file.linux-metadata.rendered
+
+  network_interface {
+    network       = google_compute_network.vpc.name
+    subnetwork    = google_compute_subnetwork.network_subnet.name
+    access_config { }
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -5,10 +5,10 @@ app_domain  = "kopicloud.com"
 environment = "dev" # Dev, Test, Prod, etc
 
 # GCP Settings
-gcp_project   = "kopicloud-medium"
+gcp_project   = "gd-gcp-gridu-cloud-cert"
 gcp_region    = "europe-west4"
 gcp_zone      = "europe-west4-b"
-gcp_auth_file = "../auth/kopicloud-medium.json"
+gcp_auth_file = "/Users/sbezanovic/Downloads/gd-gcp-gridu-cloud-cert-ab2463406125.json"
 
 # GCP Netwok
 network-subnet-cidr = "10.10.10.0/24"
